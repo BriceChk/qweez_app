3155071a83492a7d8fbabf885e37f624fcb1f68f
mickael <mickael@soloso.fr>
2021-12-14 15:17:35 +0100

    [question_appbar] add name of the qweez to the appbar

--- lib/pages/gameplay/questions/question_appbar.dart
+++ lib/pages/gameplay/questions/question_appbar.dart
@@ -16,57 +16,74 @@ class QuestionAppBar extends StatelessWidget implements PreferredSize {
   }) : super(key: key);
 
   @override
-  Size get preferredSize => const Size(double.infinity, 150);
-
-  //TODO Add quiz title in app bar (widget.qweezTitle)
+  Size get preferredSize => const Size(double.infinity, 192);
 
   @override
   Widget get child => Container(
         color: colorBlue.withOpacity(0.9),
         padding: const EdgeInsets.only(top: paddingVertical * 2),
-        child: Stack(
+        child: Column(
           children: [
-            // Scanning and enter code + title container
-            Container(
-              width: double.maxFinite,
-              margin: const EdgeInsets.only(top: paddingVertical * 2),
-              padding: const EdgeInsets.symmetric(horizontal: paddingHorizontal, vertical: paddingVertical * 2),
-              decoration: const BoxDecoration(
-                color: colorBlue,
-                borderRadius: BorderRadius.only(
-                  topLeft: Radius.circular(borderRadius),
-                  topRight: Radius.circular(borderRadius),
-                ),
-              ),
-              child: ConstrainedBox(
-                constraints: const BoxConstraints(
-                  maxHeight: 130,
-                  minHeight: 50,
-                ),
-                child: AutoSizeText(
-                  question.question,
-                  maxLines: 4,
-                  style: const TextStyle(
-                    fontSize: fontSizeTitle,
-                    color: colorWhite,
-                    fontWeight: FontWeight.w600,
-                  ),
+            Padding(
+              padding: const EdgeInsets.only(top: paddingVertical, left: paddingHorizontal, right: paddingHorizontal),
+              child: Text(
+                qweezTitle,
+                maxLines: 1,
+                overflow: TextOverflow.fade,
+                softWrap: false,
+                style: const TextStyle(
+                  color: colorWhite,
+                  fontSize: fontSizeTitle,
+                  fontWeight: FontWeight.w700,
                 ),
               ),
             ),
-            Positioned(
-              top: 15,
-              left: paddingVertical,
-              child: Container(
-                padding: const EdgeInsets.symmetric(horizontal: paddingHorizontal * 1.5, vertical: paddingVertical / 2),
-                decoration: BoxDecoration(
-                  color: colorYellow,
-                  borderRadius: BorderRadius.circular(borderRadius),
+            Stack(
+              children: [
+                // Scanning and enter code + title container
+                Container(
+                  width: double.maxFinite,
+                  margin: const EdgeInsets.only(top: paddingVertical * 2),
+                  padding: const EdgeInsets.symmetric(horizontal: paddingHorizontal, vertical: paddingVertical * 2),
+                  decoration: const BoxDecoration(
+                    color: colorBlue,
+                    borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(borderRadius),
+                      topRight: Radius.circular(borderRadius),
+                    ),
+                  ),
+                  child: ConstrainedBox(
+                    constraints: const BoxConstraints(
+                      maxHeight: 130,
+                      minHeight: 50,
+                    ),
+                    child: AutoSizeText(
+                      question.question,
+                      maxLines: 4,
+                      style: const TextStyle(
+                        fontSize: fontSizeTitle,
+                        color: colorWhite,
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                  ),
                 ),
-                child: Text(
-                  'Question ' + (index + 1).toString(),
+                Positioned(
+                  top: 15,
+                  left: paddingVertical,
+                  child: Container(
+                    padding:
+                        const EdgeInsets.symmetric(horizontal: paddingHorizontal * 1.5, vertical: paddingVertical / 2),
+                    decoration: BoxDecoration(
+                      color: colorYellow,
+                      borderRadius: BorderRadius.circular(borderRadius),
+                    ),
+                    child: Text(
+                      'Question ' + (index + 1).toString(),
+                    ),
+                  ),
                 ),
-              ),
+              ],
             ),
           ],
         ),
