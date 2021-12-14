c34bd4bd2cb8a90d7450471452aab77a504a5138
mickael <mickael@soloso.fr>
2021-12-14 17:24:19 +0100

    [join_game_page] add the form + verification + add some bubble as decoration

--- lib/pages/gameplay/join_game_page.dart
+++ lib/pages/gameplay/join_game_page.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:qweez_app/components/appbar/classic_appbar.dart';
+import 'package:qweez_app/components/form/my_text_form_field.dart';
+import 'package:qweez_app/constants.dart';
 
 class JoinGamePage extends StatefulWidget {
   final String gameCode;
@@ -13,6 +16,8 @@ class JoinGamePage extends StatefulWidget {
 //TODO Connect to socket.io, get game info, react to events, send answers to socket
 
 class _JoinGamePageState extends State<JoinGamePage> {
+  final _formKey = GlobalKey<FormState>();
+
   String _username = '';
 
   @override
@@ -25,8 +30,126 @@ class _JoinGamePageState extends State<JoinGamePage> {
 
   Widget _buildBody() {
     if (_username.isEmpty) {
-      //TODO Choose username page
-      return Container();
+      return Container(
+        color: colorWhite,
+        child: Stack(
+          fit: StackFit.expand,
+          children: [
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: paddingHorizontal),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Padding(
+                    padding: EdgeInsets.only(bottom: paddingVertical),
+                    child: Text(
+                      'Choose a username',
+                      style: TextStyle(
+                        fontSize: fontSizeSubtitle,
+                      ),
+                    ),
+                  ),
+                  Form(
+                    key: _formKey,
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        MyTextFormField(
+                          hintText: 'Username',
+                          textInputAction: TextInputAction.done,
+                          valueText: '',
+                          validator: (username) {
+                            if (username!.isEmpty) {
+                              return 'Please enter your username';
+                            }
+                          },
+                          onChanged: (username) {
+                            _username = username;
+                          },
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(top: paddingVertical),
+                          child: ElevatedButton(
+                            onPressed: () {
+                              if (_formKey.currentState!.validate()) {
+                                // TODO push à la waiting page
+                              }
+                            },
+                            child: const Padding(
+                              padding: EdgeInsets.symmetric(horizontal: paddingHorizontal),
+                              child: Text('Join'),
+                            ),
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Positioned(
+              top: 10,
+              left: 60,
+              child: Container(
+                height: 100,
+                width: 100,
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: colorYellow.withOpacity(0.2),
+                ),
+              ),
+            ),
+            Positioned(
+              top: 80,
+              right: -20,
+              child: Container(
+                height: 150,
+                width: 150,
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: colorYellow.withOpacity(0.2),
+                ),
+              ),
+            ),
+            Positioned(
+              top: 10,
+              left: -80,
+              child: Container(
+                height: 200,
+                width: 200,
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: colorBlue.withOpacity(0.2),
+                ),
+              ),
+            ),
+            Positioned(
+              right: -70,
+              bottom: -30,
+              child: Container(
+                height: 250,
+                width: 250,
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: colorBlue.withOpacity(0.2),
+                ),
+              ),
+            ),
+            Positioned(
+              left: 60,
+              bottom: 50,
+              child: Container(
+                height: 80,
+                width: 80,
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: colorYellow.withOpacity(0.2),
+                ),
+              ),
+            ),
+          ],
+        ),
+      );
     }
 
     return Container();
